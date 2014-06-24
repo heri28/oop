de06ea4bb3caccc2e3b972dd3aa9688a99147145
herytrie <hery.trie@gmail.com>
2014-06-24 11:09:49 +0700

    counter pulsa revisi (update fitur 1, fitur2)

--- Counter Pulsa/Program.cs
+++ Counter Pulsa/Program.cs
@@ -7,21 +7,25 @@ namespace Counter_Pulsa
 {
     class Program
     {
-        
-        static void Main(string[] args)
+        static void utama()
         {
-            
-            
+            //int deposit;
+            //int depositTambah;
+            //int pulsaDibeli;
+            //int pulsaJual;
+            //int hargaPulsa = 0;
+            //int laba = 0;
+            //string hp;
+
+            Stock stock = new Stock();
             Console.Clear();
-            bool cek, cek1 = true;
+            bool cek = true;
             do
             {
-                cek1 = true;
+                cek = true;
                 Console.Clear();
                 Console.Title = "Counter_Pulsa";
-                Stock stock = new Stock();
                 stock.utama();
-                Kartu_AS kartuas = new Kartu_AS();
                 Console.SetCursorPosition(5, 8); Console.WriteLine("1. Isi Pulsa Elektrik"); Console.SetCursorPosition(50, 8); Console.Write("Isi Pulsa Listrik .3");
                 Console.SetCursorPosition(5, 10); Console.Write("2. Pulsa Voucher"); Console.SetCursorPosition(50, 10); Console.Write("Pembayaran PDAM   .4");
                 Console.SetCursorPosition(50, 12); Console.Write("Lihat Laporan     .5");
@@ -29,17 +33,57 @@ namespace Counter_Pulsa
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        do
-                        {
-                            cek = true;
-                            kartuas.KartuAS();
-                        } while (cek == true);
+                       
+                            Console.Clear();
+                            stock.utama();
+                            Console.SetCursorPosition(5, 9); Console.WriteLine("1.AS");
+                            Console.SetCursorPosition(5, 11); Console.WriteLine("2.SIMPATI");
+                            Console.SetCursorPosition(5, 13); Console.WriteLine("3.XL");
+                            Console.SetCursorPosition(50, 9); Console.WriteLine("4.IM3");
+                            Console.SetCursorPosition(50, 11); Console.WriteLine("5.MENTARI");
+                            Console.SetCursorPosition(50, 13); Console.WriteLine("6.AXIS");
+                            Console.SetCursorPosition(50, 15); Console.WriteLine("0.KEMBALI");
+                            stock.jualPulsa();
+                            stock.TampilPulsa();
+                            Console.ReadKey();
+                            utama();
+                            //}
+                        break;
 
-                    break;
+                    case "2":
+                        Console.Clear();
+                        stock.utama();
+                        stock.isivoucer();
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        stock.utama();
+                        stock.TampilPulsa();
+                        Console.ReadKey();
+                        break;
+
+                    case "4":
+                        Console.Clear();
+                        stock.utama();
+                        stock.TampilPulsa();
+                        Console.ReadKey();
+                        break;
+
+                    case "5":
+                        Console.Clear();
+                        stock.utama();
+                        stock.TampilPulsa();
+                        Console.ReadKey();
+                        break;
                 }
-            } while (cek1);
+            } while (cek);
             
-                
+        }
+        
+        static void Main(string[] args)
+        {
+            utama();
         }
     }
 }
--- Counter Pulsa/Stock.cs
+++ Counter Pulsa/Stock.cs
@@ -7,7 +7,7 @@ namespace Counter_Pulsa
 {
     class Stock
     {
-        List<Kartu_AS> kartu;
+        List<pulsa> pulsa;
 
         public void utama()
         {
@@ -21,9 +21,64 @@ namespace Counter_Pulsa
 
         public Stock()
         {
-
-            kartu = new List<Kartu_AS>(); 
+            pulsa = new List<pulsa>(); 
         }
-     
+
+        public void jualPulsa()
+        {
+            pulsa.Add(new pulsa());
+            string tmp = pulsa.Last().hp;
+            if (pulsa.Count >= 2)
+                for (int i = 0; i < pulsa.Count - 1; i++)
+                    if (pulsa[i].hp == tmp)
+                    {
+                        pulsa.RemoveAt(i);
+                        break;
+                    }
+        }
+
+        public void TampilPulsa()
+        {
+            Console.WriteLine("Terdapat beberapa transaksi");
+            foreach (pulsa j in pulsa)
+                j.detailpulsa();
+        }
+
+        public void isivoucer()
+        {
+            ConsoleKeyInfo jenisVoucher;
+            Random rnd = new Random();
+            int angka = rnd.Next(100000, 999999);
+            int angka1 = rnd.Next(2000000, 8888888);
+            Console.SetCursorPosition(5, 9); Console.WriteLine("1.10000");
+            Console.SetCursorPosition(5, 11); Console.WriteLine("2.20000");
+            Console.SetCursorPosition(5, 13); Console.WriteLine("3.50000");
+            Console.SetCursorPosition(50, 9); Console.WriteLine("4.100000");
+            Console.SetCursorPosition(50, 11); Console.WriteLine("5.500000");
+            Console.SetCursorPosition(50, 13); Console.WriteLine("0.KEMBALI");
+            Console.WriteLine("");
+            Console.Write("Pilihan => ");
+            jenisVoucher = Console.ReadKey();
+            Console.WriteLine("\n");
+            switch (jenisVoucher.KeyChar)
+            {
+                case '1' :
+                    Console.WriteLine("Kode voucer pulsa 10 Ribu {0}{1} ", angka, angka1);
+                    break;
+                case '2':
+                    Console.WriteLine("Kode voucer pulsa 20 Ribu {0}{1} ", angka, angka1);
+                    break;
+                case '3':
+                    Console.WriteLine("Kode voucer pulsa 50 Ribu {0}{1} ", angka, angka1);
+                    break;
+                case '4':
+                    Console.WriteLine("Kode voucer pulsa 100 Ribu {0}{1} ", angka, angka1);
+                    break;
+                case '5':
+                    Console.WriteLine("Kode voucer pulsa 500 Ribu {0}{1} ", angka, angka1);
+                    break;
+            }
+            Console.ReadKey();
+        }
     }
 }
